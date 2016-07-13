46700fe041992325d649a09e54cf3a29e92bbbf6
satoshi <satoshi.kumano@kii.com>
2016-07-13 17:44:52 +0900

    Initial commit.

---
+++ FrameworkSample.framework/Headers/FSApi.h
@@ -0,0 +1,13 @@
+//
+//  FSApi.h
+//  FrameworkSample
+//
+//  Created by satoshi on 2016/07/13.
+//  Copyright © 2016年 Kii. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FSApi : NSObject
+- (void) doSomething;
+@end
---
+++ FrameworkSample.framework/Headers/FrameworkSample.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkSample.h
+//  FrameworkSample
+//
+//  Created by satoshi on 2016/07/13.
+//  Copyright © 2016年 Kii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameworkSample.
+FOUNDATION_EXPORT double FrameworkSampleVersionNumber;
+
+//! Project version string for FrameworkSample.
+FOUNDATION_EXPORT const unsigned char FrameworkSampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkSample/PublicHeader.h>
+#import <FrameworkSample/FSApi.h>
+
